cc04ee90608b7574c7b5e386b08acee2c3eca8f9
Benjamin Grange <benjamin.grange@epitech.eu>
2017-07-14 05:05:07 +0200

    Divided calls to kernel_init_level()

--- kernel/main.c
+++ kernel/main.c
@@ -17,8 +17,23 @@
 int
 kernel_main()
 {
-	kernel_init_level(CHAOS_INIT_LEVEL_EARLIEST, CHAOS_INIT_LEVEL_LATEST);
+	/* Super super early hooks goes first. */
+	kernel_init_level(CHAOS_INIT_LEVEL_EARLIEST, CHAOS_INIT_LEVEL_ARCH_EARLY - 1);
+
+	/* Then goes early arch and platform stuff */
+	kernel_init_level(CHAOS_INIT_LEVEL_ARCH_EARLY, CHAOS_INIT_LEVEL_PLATFORM_EARLY - 1);
+	kernel_init_level(CHAOS_INIT_LEVEL_PLATFORM_EARLY, CHAOS_INIT_LEVEL_ARCH - 1);
+
+	/* Initialize the memory management */
 	pmm_init();
+
+	/* It's time to initialize arch and platform */
+	kernel_init_level(CHAOS_INIT_LEVEL_ARCH, CHAOS_INIT_LEVEL_PLATFORM - 1);
+	kernel_init_level(CHAOS_INIT_LEVEL_PLATFORM, CHAOS_INIT_LEVEL_LATEST);
+
+	/* Drivers hooks would go there */
+
+	/* We're now ready to go on */
 	printf("\nWelcome to ChaOS\n\n");
 	return (0);
 }
